d7822f70495511d291044359532391940b9d758f
Hörður <hordur@agr.is>
2013-09-20 18:00:29 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ALiCCSapp::Application.config.secret_token = '6ff7ade5c4509576d67c35a01666a914d2b752aeef80dffc36751685189a409774271834d62413ea962fd4c9e74318abc26ecabaffe5a9c11a3f816de308419a'
