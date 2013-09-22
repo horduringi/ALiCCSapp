55ddb3c2feece654fd436e9038c311e7ab3b5d56
Hörður <hordur@agr.is>
2013-09-22 22:58:00 +0000

    Delete some unused migrations

--- db/migrate/20130922150002_add_schema_from_a_li_ccs.rb
+++
@@ -1,4 +0,0 @@
-class AddSchemaFromALiCcs < ActiveRecord::Migration
-  def change
-  end
-end
--- db/migrate/20130922152619_create_anthracyclines_bolus_infs.rb
+++
@@ -1,9 +0,0 @@
-class CreateAnthracyclinesBolusInfs < ActiveRecord::Migration
-  def change
-    create_table :anthracyclines_bolus_infs do |t|
-      t.string :text
-
-      t.timestamps
-    end
-  end
-end
